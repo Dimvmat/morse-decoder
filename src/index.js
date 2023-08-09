ac0659d5e2d955b4968d922059cc460ce4285051
dimvmat <102551294+Dimvmat@users.noreply.github.com>
2023-08-09 22:43:06 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,44 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arraySymbol = [];
+    let symbol;
+    let arrayText = [];
+
+    for (let i = 0; i < expr.length; i += 10) {
+        symbol = expr.slice(i, i + 10);
+        arraySymbol.push(symbol);
+    }
+
+    arraySymbol.forEach(function (symbol) {
+        let arrayPair = [];
+
+        for (let j = 0; j < 10; j += 2) {
+            let pair = symbol.slice(j, j + 2);
+
+            if (pair == '10') {
+                arrayPair.push('.');
+            } else if (pair == '11') {
+                arrayPair.push('-');
+            } else if (pair == '**') {
+                arrayPair.push(pair);
+            }
+        }
+
+        str = arrayPair.join('');
+
+        for (let [prop, value] of Object.entries(MORSE_TABLE)) {
+            if (prop === str) {
+                arrayText.push(value);
+            }
+        }
+
+        if (str[0] === '*') {
+            arrayText.push(' ');
+        }
+    })
+
+    return arrayText.join('');
 }
 
 module.exports = {
